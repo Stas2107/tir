223a1195b300da62df2f389082a678c8877e9aa9
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-03-22 18:00:05 +0400

    screen_const

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 pygame.init()
 
+SCREEN_HEIGHT = 600
+SCREEN_WIGTH = 800
+screen = pygame.display.set_mode((SCREEN_WIGTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра ТИР")
+icon = pygame.image.load("/img/icon1.jpg")
+pygame.display.set_icon(icon)
+
+
 running = True
 while running:
     pass
